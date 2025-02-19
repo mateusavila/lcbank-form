1cae5b57f2f358663b3333dc005b173bb3302e66
mateusavila <mateus@mateusavila.com.br>
2025-02-19 14:25:04 -0300

    feat: validating name with space with console

--- src/validateName.ts
+++ src/validateName.ts
@@ -1,7 +1,6 @@
 export function validateName(name: string): boolean {
   const trimmedName = name.trim();
-
-  // Verifica se há pelo menos um espaço e se as palavras têm mais de um caractere
   const parts = trimmedName.split(' ');
+  console.log(parts.length >= 2 && parts.every(part => part.length > 1))
   return parts.length >= 2 && parts.every(part => part.length > 1);
 }
