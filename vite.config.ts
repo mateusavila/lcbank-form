ff46b30c8ddde6c00fce14703ff6794b3a668e5e
mateusavila <mateus@mateusavila.com.br>
2025-09-29 18:37:39 -0300

    feat: add test

--- vite.config.ts
+++ vite.config.ts
@@ -1,36 +1,4 @@
-// import { defineConfig } from 'vite'
-
-// export default defineConfig(({ mode }) => {
-
-//   if (mode === 'production') {
-//     return {
-//       build: {
-//         rollupOptions: {
-//           output: {
-//             entryFileNames: 'form-lcbank.js',
-//             assetFileNames: (assetInfo) => {
-//               if (assetInfo.name!.endsWith('.css')) {
-//                 return 'form-lcbank.css'
-//               }
-//               return '[name].[hash][extname]'
-//             }
-//           }
-//         }
-//       }
-//     }
-//   }
-//   return {
-//     test: {
-//       includeSource: ['src/**/*.{js,ts}'],
-//     },
-//     define: {
-//       'import.meta.vitest': 'undefined',
-//     }
-//   }
-// })
-
 import { defineConfig } from 'vite'
-
 export default defineConfig(({ mode }) => {
   if (mode === 'production') {
     return {
